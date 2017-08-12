5b7d1d2f1c44c0a8639b4024e05c8ec86b6e8237
Alex Stroulger <alexstroulger@gmail.com>
2017-08-11 20:25:58 -0400

    [FEATURE] Handles machine learning for image stream

---
+++ LiveCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  LiveCameraViewController.h
+//  tf_simple_example
+//
+//  Created by Alexandra Stroulger on 2017-08-11.
+//  Copyright © 2017 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiveCameraViewController : UIViewController
+
+@end
